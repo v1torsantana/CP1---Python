b533503510e8402f9ffba43f4981c0d509fbe4e6
Vitor Sant'ana <vitor.ssantana07@gmail.com>
2023-04-12 22:02:03 -0300

    python

---
+++ main.py
@@ -0,0 +1,16 @@
+#Checkpoint 1 - Python - VITOR DA SILVA SANT'ANA - ESPY - RM 99586
+print('Seja bem vindo à loja Agnello!')
+nome = str(input('Por favor digite o seu nome:'))
+idade = int(input('Por favor digite a sua idade:'))
+enderecocli = str(input('Por favor digite o seu endereço residencial:'))
+enderecoent = str(input('Por favor digite o seu endereço de entrega:'))
+
+if idade < 18:
+    print('Infelizmente você não pode continuar comprando conosco, nossos produtos são vendidos apenas para maiores de 18 anos! A compra foi finalizada.')
+
+else:
+
+
+
+
+
